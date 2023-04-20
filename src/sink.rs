22dc4fba2bd4d9597553fc643914f52f57840090
Denis Carriere <carriere.denis@gmail.com>
2023-04-19 22:29:22 -0400

    Add location

--- src/sink.rs
+++ src/sink.rs
@@ -20,6 +20,7 @@ pub fn prom_out(block: Block) -> Result<PrometheusOperations, Error> {
             if trace.receiver != account { continue; }
             if account != "iot.taiss" { continue; }
 
+            // Temperature
             let temperature_data = abi::Temperature::try_from(action_trace.json_data.as_str());
             match temperature_data {
                 Ok(temperature_data) => {
@@ -27,6 +28,22 @@ pub fn prom_out(block: Block) -> Result<PrometheusOperations, Error> {
                     let temperature: f64 = temperature_data.temperature.parse().unwrap();
                     let device_label = HashMap::from([("device_id".to_string(), device_id.to_string())]);
                     prom_out.push(Gauge::from("temperature").with(device_label).set(temperature));
+                },
+                Err(_) => {}
+            }
+
+            // Location
+            let location_data = abi::Location::try_from(action_trace.json_data.as_str());
+            match location_data {
+                Ok(location_data) => {
+                    let device_id = location_data.device_id;
+                    let x: f64 = location_data.x.parse().unwrap();
+                    let y: f64 = location_data.x.parse().unwrap();
+                    let z: f64 = location_data.x.parse().unwrap();
+                    let device_label = HashMap::from([("device_id".to_string(), device_id.to_string())]);
+                    prom_out.push(Gauge::from("x").with(device_label.clone()).set(x));
+                    prom_out.push(Gauge::from("y").with(device_label.clone()).set(y));
+                    prom_out.push(Gauge::from("z").with(device_label.clone()).set(z));
                 },
                 Err(_) => {}
             }
